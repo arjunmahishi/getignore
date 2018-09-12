637d593d46914fe269b5300eb44676131d8ab4f9
Arjun Mahishi <arjun.mahishi@gmail.com>
2018-09-12 21:18:59 +0530

    Working with file name given as the first arg

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const axios = require("axios")
 const cheerio = require("cheerio")
 
+args = process.argv
+
+if (args.length < 3) {
+	console.log("Please mention a .gitignore name")
+	process.exit()
+}
+
+// console.log("Looking for the appropriate .gitignore file")
 axios.get("https://github.com/github/gitignore").then(res => {
-	ignores = parsePage(res.data)
-	console.log(ignores.length)
+	ignores = parsePage(res.data).filter(ign => ign.toLowerCase().includes(args[2].toLowerCase()))
+	if (ignores[0]) {
+		// console.log("Found: ", ignores[0], ", fetching its contents")
+		axios.get(`https://raw.githubusercontent.com/github/gitignore/master/${ignores[0]}`).then(file => {
+			console.log(file.data)
+		})
+	}else{
+		console.log("Couldn't find it. Sorry!")
+	}
 })
 
 const parsePage = (html) => {
